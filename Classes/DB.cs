9c85e15afa0f9e6c6e2c6e87c67fbc1179ecef64
lucas.lopes@devsoftbr.com <lucas.lopes@devsoftbr.com>
2021-07-15 11:38:06 -0300

    Update

---
+++ Classes/DB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Sistema_Caixa_Seguro.Classes
+{
+    class DB
+    {
+        private static string dbpath = "";
+        public async static void Open_db()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("database.db", CreationCollisionOption.OpenIfExists);
+            dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "database.db");
+        }
+
+        public static SqliteCommand ExecuteCommand(String command)
+        {
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                SqliteCommand createTable = new SqliteCommand(command, db);
+
+                return createTable;
+            }
+        }
+    }
+}
---
+++ Classes/loadDB.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Sistema_Caixa_Seguro.Classes
+{
+    class LoadDB
+    {
+        public static void Acessos()
+        {
+            String command = "CREATE TABLE IF NOT EXISTS `TipoAcesso` (" +
+                "`idTipoAcesso` INTEGER NOT NULL UNIQUE," +
+                "`NomeAcesso` TEXT NOT NULL, " +
+                "PRIMARY KEY('idTipoAcesso' AUTOINCREMENT));";
+
+            SqliteCommand cmd = DB.ExecuteCommand(command);
+
+            cmd.Connection.Open();
+
+            cmd.ExecuteNonQuery();
+
+            command = "INSERT INTO 'TipoAcesso'(NomeAcesso)" +
+                "SELECT 'Vendedor' " +
+                "WHERE NOT EXISTS(SELECT * FROM 'TipoAcesso' WHERE NomeAcesso = 'Vendedor');";
+
+            cmd.CommandText = command;
+            cmd.ExecuteNonQuery();
+
+            command = "INSERT INTO 'TipoAcesso'(NomeAcesso)" +
+                "SELECT 'Gerente' " +
+                "WHERE NOT EXISTS(SELECT * FROM 'TipoAcesso' WHERE NomeAcesso = 'Gerente');";
+
+            cmd.CommandText = command;
+            cmd.ExecuteNonQuery();
+
+            cmd.Connection.Close();
+        }
+
+        public static void Funcionarios()
+        {
+            String command = "CREATE TABLE IF NOT EXISTS 'Funcionario'(" +
+                "'idFuncionario' INTEGER NOT NULL UNIQUE," +
+                "'NomeFuncionario'   TEXT NOT NULL," +
+                "'SenhaFuncionario' TEXT NOT NULL," +
+                "'idTipoAcesso'  INTEGER NOT NULL," +
+                "'DataCadastroFuncionario' TEXT NOT NULL, " +
+                "PRIMARY KEY('idFuncionario' AUTOINCREMENT) " +
+                "FOREIGN KEY(idTipoAcesso) REFERENCES TipoAcesso(idTipoAcesso));";
+
+            SqliteCommand cmd = DB.ExecuteCommand(command);
+
+            cmd.Connection.Open();
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Connection.Close();
+        }
+
+        public static void CodBarras()
+        {
+            string command = "CREATE TABLE IF NOT EXISTS `CodBarProduto` (" +
+                "`idCodBarProduto` INTEGER NOT NULL UNIQUE," +
+                "`idProduto` INTEGER NOT NULL, " +
+                "`CodBar` TEXT NOT NULL, " +
+                "PRIMARY KEY('idCodBarProduto' AUTOINCREMENT) );";
+
+            SqliteCommand cmd = DB.ExecuteCommand(command);
+
+            cmd.Connection.Open();
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Connection.Close();
+        }
+
+        public static void CategoriaProdutos()
+        {
+            string command = "CREATE TABLE IF NOT EXISTS `CategoriaProdutos` (" +
+                "`idCategoria` INTEGER NOT NULL UNIQUE," +
+                "`NomeCategoria` TEXT NOT NULL, " +
+                "`DescricaoCategoria` TEXT NOT NULL, " +
+                "PRIMARY KEY('idCategoria' AUTOINCREMENT) );";
+
+            SqliteCommand cmd = DB.ExecuteCommand(command);
+
+            cmd.Connection.Open();
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Connection.Close();
+        }
+    }
+}
